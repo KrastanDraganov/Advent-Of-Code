34a298ecb331e96d108e5611220910c1e4772b05
Krastan Draganov <krastan.draganov@trading212.com>
2023-12-16 22:38:36 +0200

    Adding template for c++

---
+++ 2023/c++/template.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <fstream>
+
+#define endl '\n'
+
+using namespace std;
+
+int part1()
+{
+    return -1;
+}
+
+int part2()
+{
+    return -1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ifstream fin("input.txt");
+
+    fin.close();
+
+    auto t_begin = chrono::high_resolution_clock::now();
+    cout << "Day X, part 1: " << part1() << endl;
+    auto t_end = chrono::high_resolution_clock::now();
+
+    auto part1Time = chrono::duration<double, std::milli>(t_end - t_begin).count();
+
+    t_begin = chrono::high_resolution_clock::now();
+    cout << "Day X, part 2: " << part2() << endl;
+    t_end = chrono::high_resolution_clock::now();
+
+    auto part2Time = chrono::duration<double, std::milli>(t_end - t_begin).count();
+
+    cout << "Completed part 1 in: " << part1Time << " ms" << endl;
+    cout << "Completed part 2 in: " << part2Time << " ms" << endl;
+}
