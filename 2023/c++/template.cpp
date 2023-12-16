5d93f91a1e75a26b28274fb39863b1f12c2dbf67
Krastan Draganov <krastan.draganov@trading212.com>
2023-12-16 23:50:47 +0200

    Updating 2023 C++ template

--- 2023/c++/template.cpp
+++ 2023/c++/template.cpp
@@ -26,17 +26,19 @@ int main()
     fin.close();
 
     auto t_begin = chrono::high_resolution_clock::now();
-    cout << "Day X, part 1: " << part1() << endl;
+    cout << "Result, part 1: " << part1() << endl;
     auto t_end = chrono::high_resolution_clock::now();
 
     auto part1Time = chrono::duration<double, std::milli>(t_end - t_begin).count();
 
     t_begin = chrono::high_resolution_clock::now();
-    cout << "Day X, part 2: " << part2() << endl;
+    cout << "Result, part 2: " << part2() << endl;
     t_end = chrono::high_resolution_clock::now();
 
     auto part2Time = chrono::duration<double, std::milli>(t_end - t_begin).count();
 
     cout << "Completed part 1 in: " << part1Time << " ms" << endl;
     cout << "Completed part 2 in: " << part2Time << " ms" << endl;
+
+    return 0;
 }
